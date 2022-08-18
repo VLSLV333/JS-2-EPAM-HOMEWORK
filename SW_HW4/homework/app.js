b3358eebe452ceee21c7c2b0455d8360dcce1034
VLSLV333 <v.sherstyk@gmail.com>
2022-08-18 06:13:02 +0000

    done with map

--- SW_HW4/homework/app.js
+++ SW_HW4/homework/app.js
@@ -24,28 +24,49 @@
 //   }
 // }
 
-function forEach(arr, func) {
+function forEach(arr, func){
   for (let i = 0; i < arr.length; i++) {
    func(arr[i]);
   }
 }
+// function map(arr, mapFunc) {
+//   const mapArr = [];
+//   for (let item of arr) {
+//     const result = mapFunc(item);
+//     mapArr.push(result);
+//   }
+//   console.log(mapArr);
+// }
+function map(array, callback) {
+  const result = [];
+  function addModifiedElToArray(el) {
+    result.push(callback(el));
+  }
+  forEach(array, addModifiedElToArray);
+  return result;
+}
+console.log(map([1, 2, 3, 4, 5], function (el) { return el * 2; }))
+
+
+
+
 // function map(arr, func) {
 //   const results = [];
 //   forEach(arr, func);
 //   return results;
 // }
-function map(arr, mapFunc){    
-const mapArr = [];  
-  for(let item of arr) {
-    const result = mapFunc(item);        
-    mapArr.push(result);}    
-    return mapArr;
-}
+// function map(arr, mapFunc){    
+// const mapArr = [];  
+//   for(let item of arr) {
+//     const result = mapFunc(item);        
+//     mapArr.push(result);}    
+//     return mapArr;
+// }
 
 // function add (part){
 //   return part += 3
 // }
-console.log(map([1, 2, 3, 4, 5], function (el) { return el * 2; }))
+
 
 
 // function filter(arr, func) {
