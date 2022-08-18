86a7f746eb348b2320d0b40a55b9299b4624cee8
VLSLV333 <v.sherstyk@gmail.com>
2022-08-18 10:47:20 +0000

    4 and 5 are done

--- SW_HW4/homework/app.js
+++ SW_HW4/homework/app.js
@@ -18,64 +18,92 @@
 //   }
 // }
 
-// function forEach(arr, func) {
-//   for (let item of arr) {
-//    func(item);
-//   }
-// }
-
-function forEach(arr, func){
-  for (let i = 0; i < arr.length; i++) {
-   func(arr[i]);
+function forEach(arr, func) {
+  for (let item of arr) {
+   func(item);
   }
 }
-// function map(arr, mapFunc) {
-//   const mapArr = [];
-//   for (let item of arr) {
-//     const result = mapFunc(item);
-//     mapArr.push(result);
-//   }
-//   console.log(mapArr);
-// }
-function map(array, callback) {
+
+function map(arr, func) {
   const result = [];
   function addModifiedElToArray(el) {
-    result.push(callback(el));
+    result.push(func(el));
   }
-  forEach(array, addModifiedElToArray);
+  forEach(arr, addModifiedElToArray);
   return result;
 }
-console.log(map([1, 2, 3, 4, 5], function (el) { return el * 2; }))
 
 
+function filter(arr, func) {
+  const result = [];
+  function addFilteredElToArray(el) {
+    if (func(el) === true){
+      result.push(el);
+    } 
+  }
+  forEach(arr, addFilteredElToArray);
+  return result;
+}
 
+// let data = 
+//   [
+//     {
+//     "_id": "5b5e3168c6bf40f2c1235cd6",
+//     "index": 0,
+//     "age": 39,
+//     "eyeColor": "green",
+//     "name": "Stein",
+//     "favoriteFruit": "apple"
+//     },
+//     {
+//     "_id": "5b5e3168e328c0d72e4f27d8","index": 1,
+//     "age": 38,
+//     "eyeColor": "blue",
+//     "name": "Cortez",
+//     "favoriteFruit": "strawberry"
+//     },
+//     {
+//     "_id": "5b5e3168cc79132b631c666a",
+//     "index": 2,
+//     "age": 2,
+//     "eyeColor": "blue",
+//     "name": "Suzette",
+//     "favoriteFruit": "apple"
+//     },
+//     {
+//     "_id": "5b5e31682093adcc6cd0dde5",
+//     "index": 3,
+//     "age": 17,
+//     "eyeColor": "green",
+//     "name": "Weiss",
+//     "favoriteFruit": "banana"
+//     },
+//     {
+//     "_id": "5b5e3168209323123zxcc6cd0dde5",
+//     "index": 6,
+//     "age": 20,
+//     "eyeColor": "green",
+//     "name": "Yuliana",
+//     "favoriteFruit": "strawberry"
+//     }
+//     ]
 
-// function map(arr, func) {
-//   const results = [];
-//   forEach(arr, func);
-//   return results;
-// }
-// function map(arr, mapFunc){    
-// const mapArr = [];  
-//   for(let item of arr) {
-//     const result = mapFunc(item);        
-//     mapArr.push(result);}    
-//     return mapArr;
-// }
+function getAdultAppleLovers(data) {
+  let requiredAge = 18;
+  function requirmentsForObjects (el) {
+    if (el['favoriteFruit'] === 'apple' && el['age'] > requiredAge){
+      return true;
+    }
+  };
+  objectsWhichFit = filter(data, requirmentsForObjects);
+  function whichParametrToReturn (el){
+    return el.name;
+  }
+ finalAnswer = map(objectsWhichFit, whichParametrToReturn);
+  return finalAnswer
+}
+// console.log(getAdultAppleLovers(data))
 
-// function add (part){
-//   return part += 3
-// }
-
-
-
-// function filter(arr, func) {
-
-// }
-
-// function getAdultAppleLovers(data) {
-
-// }
 
 // function getKeys(obj) {
 
@@ -85,3 +113,9 @@ console.log(map([1, 2, 3, 4, 5], function (el) { return el * 2; }))
 
 // }
 
+
+// function forEach(arr, func){
+//   for (let i = 0; i < arr.length; i++) {
+//    func(arr[i]);
+//   }
+// }
--- tets/app.js
+++ tets/app.js
@@ -2,17 +2,48 @@ function reverseNumber(num) {
 
 }
 
-function forEach(arr, func) {
+// function forEach(arr, func) {
 
-}
-
-function map(arr, func) {
-
-}
-
-function filter(arr, func) {
-
-}
+// }
+let test = [
+    {
+    "_id": "5b5e3168c6bf40f2c1235cd6",
+    "index": 0,
+    "age": 39,
+    "eyeColor": "green",
+    "name": "Stein",
+    "favoriteFruit": "apple"
+    },
+    {
+    "_id": "5b5e3168e328c0d72e4f27d8","index": 1,
+    "age": 38,
+    "eyeColor": "blue",
+    "name": "Cortez",
+    "favoriteFruit": "strawberry"
+    },
+    {
+    "_id": "5b5e3168cc79132b631c666a",
+    "index": 2,
+    "age": 2,
+    "eyeColor": "blue",
+    "name": "Suzette",
+    "favoriteFruit": "apple"
+    },
+    {
+    "_id": "5b5e31682093adcc6cd0dde5",
+    "index": 3,
+    "age": 17,
+    "eyeColor": "green",
+    "name": "Weiss",
+    "favoriteFruit": "banana"
+    }
+  ]
+  console.log(test)
+  function getAdultAppleLovers(data) {
+    
+  }
+  
+  // console.log(getAdultAppleLovers(test))
 
 function getAdultAppleLovers(data) {
 
